71364b88c7353138801ef99184a66d8d00abafb5
Marwa Elali <marwaelali@gmail.com>
2018-05-06 18:21:11 +0100

    4 - add the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+  it("has an initial balance of 0") do
+    expect(subject.balance).to eq(0)
+  end
 end
