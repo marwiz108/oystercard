bf8b721d4d09d332752cf52d938cc4d487eb85d3
Marwa Elali <marwaelali@gmail.com>
2018-06-14 16:16:22 +0100

    refactor oystercard tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,21 +7,23 @@ describe Oystercard do
   let(:exit_station) { double :station }
   let(:journey) { {entry: entry_station, exit: exit_station} }
 
-  it 'has an initial balance of 0' do
-    expect(subject.balance).to eq(0)
-  end
-
-  describe 'card balance' do
-    before(:each) do
-      subject.top_up(Oystercard::TOPUP_LIMIT)
+  describe 'balance' do
+    it 'has an initial balance of 0' do
+      expect(subject.balance).to eq(0)
     end
 
-    it 'adds money to the card' do
-      expect(subject.balance).to eq(90)
-    end
+    describe 'top up' do
+      before(:each) do
+        subject.top_up(Oystercard::TOPUP_LIMIT)
+      end
 
-    it 'throws error message if balance exceeds limit' do
-      expect { subject.top_up(5) }.to raise_error("Top up limit of #{Oystercard::TOPUP_LIMIT} reached")
+      it 'adds money to the card' do
+        expect(subject.balance).to eq(90)
+      end
+
+      it 'throws error message if balance exceeds limit' do
+        expect { subject.top_up(5) }.to raise_error("Top up limit of #{Oystercard::TOPUP_LIMIT} reached")
+      end
     end
   end
 
@@ -30,7 +32,7 @@ describe Oystercard do
       subject.top_up(Oystercard::TOPUP_LIMIT)
     end
 
-    describe '.in_journey?' do
+    describe '#in_journey?' do
       it 'starts not in journey' do
         expect(subject).not_to be_in_journey
       end
@@ -47,7 +49,7 @@ describe Oystercard do
       end
     end
 
-    describe '.touch_in' do
+    describe '#touch_in' do
       it 'stores entry station at touch in' do
         subject.touch_in(station)
         expect(subject.entry_station).to eq(station)
@@ -59,7 +61,7 @@ describe Oystercard do
       end
     end
 
-    describe '.touch_out' do
+    describe '#touch_out' do
       it 'stores exit station' do
         subject.touch_in(entry_station)
         subject.touch_out(exit_station)
